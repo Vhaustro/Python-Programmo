f0dd6ed5a850dc18d334e7e93b8a86d73e92b687
unknown <mandenno228@gmail.com>
2021-07-13 12:55:11 +0300

    update

--- Greetin.py
+++ Greetin.py
@@ -1,6 +1,5 @@
 # Second Part of the Programme am Making
 
-
 print ('Hello New User')
 print ('My name is Vhaustro and I am your Personal Assistant,')
 print ('I shall help you organize your life.')
@@ -16,9 +15,9 @@ print ('Before we proceed I have one question,')
 
 gender = input ('Are you Male or Female?')
 
-if gender.lower == 'Male':
-    print ('Salutations ' + name)
+if gender == 'male' :
+    print ('Nice Sir' + name)
 else:
-    print ('It is a pleasure to meet you, Mrs.' + name )
+    print ('Good Mrs.' + name)
 
 print ('Thankyou, I believe we shall both enjoy this experience ')
